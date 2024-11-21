c4c103b9ff4ccf68d5b0f4e7ec689732472a0ceb
SzymonN <szymonnajdzion@gmail.com>
2024-11-21 08:51:04 +0100

    zad1 nie w tym miejscu co trzeba /wersja robocza js/

---
+++ zad1/functions.js
@@ -0,0 +1,18 @@
+isOdd = function(x){
+    if(x%2 == 0){
+        return false;
+    }
+    else{
+        return true;
+    }
+}
+
+isEven = (x) => {
+    if(x%2 == 0){
+        return false;
+    }
+    else{
+        return true;
+    }
+}
+
